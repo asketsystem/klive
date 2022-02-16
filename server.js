01ddcdc21d0512d904d3446114071b00321d1b74
asketsystem <asketsystem1@gmail.com>
2022-02-16 19:54:47 +0000

    Updates

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", function (req, res) {
   res.render("room");
 });
 
 server.listen(3030);
-
-app.set("view engine", "ejs");
