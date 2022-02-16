d7d3ede8d2c58f64d7b0e5eb852878f80d3d8aac
asketsystem <asketsystem1@gmail.com>
2022-02-16 20:05:59 +0000

    VideoStream script

--- public/script.js
+++ public/script.js
@@ -11,3 +11,11 @@ navigator.mediaDevices
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
   });
+
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+    videoGrid.append(video);
+  });
+};
