ba1a933662ed0c29a5335c662572a8184a061ee0
asketsystem <asketsystem1@gmail.com>
2022-02-16 19:59:23 +0000

    Adding uuid for generating unique id for each user

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
 
-app.get("/", function (req, res) {
-  res.render("room");
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.param.room });
 });
 
 server.listen(3030);
